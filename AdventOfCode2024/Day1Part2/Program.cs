926670b19280e6db96271ac9a7a5dcb314fc625d
jvk1993 <Jeroen.vankoot@hotmail.nl>
2024-12-06 22:20:20 +0100

    Solved day 1 part 2

---
+++ AdventOfCode2024/Day1Part2/Program.cs
@@ -0,0 +1,30 @@
+﻿string[] fileContent = await ReadFileContent();
+
+List<int> left = [];
+List<int> right = [];
+
+foreach (var line in fileContent)
+{
+    string[] numbers = line.Split("   ");
+    left.Add(int.Parse(numbers.First()));
+    right.Add(int.Parse(numbers.Last()));
+}
+
+var groupedRight = right.GroupBy(row => row).ToDictionary(row => row.Key, row => row.Count());
+int similarity = 0;
+foreach(int item in left)
+{
+    groupedRight.TryGetValue(item, out int count);
+    if(count > 0)
+    {
+        similarity += item * count;
+    }
+}
+
+Console.WriteLine(similarity);
+Console.ReadLine();
+
+static async Task<string[]> ReadFileContent()
+{
+    return await File.ReadAllLinesAsync("C:\\Users\\jeroe\\Documents\\dev\\AdventOfCode2024\\AdventOfCode2024\\Day1\\input\\Day1Input.txt");
+}
