c1f6296884a8d9abc9c7a8109f73631dcc5d77d5
jvk1993 <Jeroen.vankoot@hotmail.nl>
2024-12-06 22:03:11 +0100

    First day solution

--- AdventOfCode2024/Day1/Program.cs
+++ AdventOfCode2024/Day1/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] fileContent = await ReadFileContent();
+
+List<int> left = [];
+List<int> right = [];
+
+foreach (var line in fileContent)
+{
+    string[] numbers = line.Split("   ");
+    left.Add(int.Parse(numbers.First()));
+    right.Add(int.Parse(numbers.Last()));
+}
+
+left.Sort();
+right.Sort();
+
+Func<int, int, int> CalculatedDifference = (firstNumber, secondNumber) =>
+{
+    if (firstNumber < secondNumber)
+    {
+        return secondNumber - firstNumber;
+    }
+    return firstNumber - secondNumber;
+};
+
+var zippedList = left.Zip(right, (first, second) => CalculatedDifference(first, second));
+Console.WriteLine(zippedList.Sum());
+Console.ReadLine();
+
+static async Task<string[]> ReadFileContent()
+{
+    return await File.ReadAllLinesAsync("C:\\Users\\jeroe\\Documents\\dev\\AdventOfCode2024\\AdventOfCode2024\\Day1\\input\\Day1Input.txt");
+}
